a0f653a83af2de352ffc77229f7c98b06469becd
Luke Oliff <luke@lukeoliff.com>
2018-01-25 08:35:30 +0000

    Stamp the guide

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -10,30 +10,31 @@ import { rhythm } from '../utils/typography'
 class Bio extends React.Component {
   render() {
     return (
-      <div
-        style={{
-          display: 'flex',
-          marginBottom: rhythm(2.5),
-        }}
-      >
-        <img
-          src={profilePic}
-          alt={`Kyle Mathews`}
-          style={{
-            marginRight: rhythm(1 / 2),
-            marginBottom: 0,
-            width: rhythm(2),
-            height: rhythm(2),
-          }}
-        />
-        <p>
-          Written by <strong>Kyle Mathews</strong> who lives and works in San
-          Francisco building useful things.{' '}
-          <a href="https://twitter.com/kylemathews">
-            You should follow him on Twitter
-          </a>
-        </p>
-      </div>
+        <div
+            style={{
+                display: 'flex',
+                marginBottom: rhythm(2.5),
+            }}
+        >
+            <img
+                src={profilePic}
+                alt={`Kyle Mathews`}
+                style={{
+                    marginRight: rhythm(1 / 2),
+                    marginBottom: 0,
+                    width: rhythm(3),
+                    height: rhythm(3),
+                    borderRadius: '50%'
+                }}
+            />
+            <p>
+                <strong>Luke Oliff</strong> is a Technical Writer working for <a href="https://auth0.com">Auth0</a> in the UK.
+                <br/>
+                <a href="https://twitter.com/mroliff">
+                    Follow him on Twitter
+                </a>
+            </p>
+        </div>
     )
   }
 }
