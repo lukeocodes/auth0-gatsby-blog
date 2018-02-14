8542d3d8e35283b5e752d313ac540f028bd0689e
Luke Oliff <luke@lukeoliff.com>
2018-02-14 19:13:06 +0000

    Remove history

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,8 +1,5 @@
 import auth0 from 'auth0-js';
-import createBrowserHistory from 'history/createBrowserHistory';
 import { navigateTo } from "gatsby-link";
-
-const history = createBrowserHistory();
 
 export default class Auth {
   auth0 = new auth0.WebAuth({
